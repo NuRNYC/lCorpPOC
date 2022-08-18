4a3cdf91e22edf1df85ca9f26120fc6d431cd589
NuRNYC <asmnuralam456@gmail.com>
2022-08-17 21:07:24 -0400

    init
    init

---
+++ com.lcorp.testNG.selenium/src/main/java/com/lcorp/testNG/selenium/com/lcorp/testNG/selenium/App.java
@@ -0,0 +1,13 @@
+package com.lcorp.testNG.selenium.com.lcorp.testNG.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.lcorp.testNG.selenium/src/test/java/com/lcorp/testNG/selenium/com/lcorp/testNG/selenium/AppTest.java
@@ -0,0 +1,20 @@
+package com.lcorp.testNG.selenium.com.lcorp.testNG.selenium;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
