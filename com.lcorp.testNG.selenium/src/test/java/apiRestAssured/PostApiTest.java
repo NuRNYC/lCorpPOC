cb01879b26f50abf574556e74715f29a5492f699
NuRNYC <asmnuralam456@gmail.com>
2022-08-22 08:52:20 -0400

    updated
    updated

--- com.lcorp.testNG.selenium/src/test/java/apiRestAssured/PostApiTest.java
+++ com.lcorp.testNG.selenium/src/test/java/apiRestAssured/PostApiTest.java
@@ -24,9 +24,59 @@ public class PostApiTest {
 	public static JSONObject requestParam;
 	public Logger logger;
 	String emp_firtName="TestData12345";
+	String emp_id="";
+	
 	public static HashMap map=new HashMap();
 	public static HashMap map1=new HashMap();
 	
+	
+	
+	String bURI="https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1/";
+	String bPath="/Users";
+	
+	
+	
+	//RestAssured.baseURI="https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1/";   
+	//RestAssured.basePath="";
+	
+	@BeforeClass
+	public void postRecord1() {
+		requestParam = new JSONObject();
+		requestParam.put("createdAt" , "1631825833");
+		requestParam.put("employee_firstname", "TestData12345");
+		requestParam.put("employee_lastname", "TestData12345");
+		requestParam.put("employee_phonenumbe", "264-783-9453");
+		requestParam.put("ademployee_emaildress", "ademployee_emaildress 1");
+		requestParam.put("citemployee_addressy", "citemployee_addressy 1");
+		requestParam.put("stateemployee_dev_level", "stateemployee_dev_level 1");
+		requestParam.put("employee_gender", "employee_gender 1");
+		requestParam.put("employee_hire_date", "2025-10-31T16:35:45.426Z");
+		requestParam.put("employee_onleave", "true");
+
+		JSONArray techStackList= new JSONArray();
+		JSONArray projectList= new JSONArray();
+		
+		requestParam.put("tech_stack", techStackList);
+		requestParam.put("project", projectList);
+		
+
+		RestAssured.baseURI=bURI;;   
+		RestAssured.basePath=bPath;
+		
+		Response response =given()
+			.header("Accept-Encoding", "gzip, deflate, br")
+			.contentType("application/json")
+			.body(requestParam)
+		.when()
+			.post()
+		.then()
+			.statusCode(201)
+			.log().all().extract().response();
+		
+		emp_id=response.jsonPath().getString("id");
+		System.out.println("Employee ID : "+emp_id);
+		
+	}
 	
 	@SuppressWarnings("unchecked")
 	@Test 
@@ -39,7 +89,7 @@ public class PostApiTest {
 		requestParam.put("employee_firstname", "TestData12345");
 		requestParam.put("employee_lastname", "TestData12345");
 		requestParam.put("employee_phonenumbe", "264-783-9453");
-		requestParam.put("employee_emaildress", "employee_emaildress 1");
+		requestParam.put("ademployee_emaildress", "ademployee_emaildress 1");
 		requestParam.put("citemployee_addressy", "citemployee_addressy 1");
 		requestParam.put("stateemployee_dev_level", "stateemployee_dev_level 1");
 		requestParam.put("employee_gender", "employee_gender 1");
@@ -54,6 +104,7 @@ public class PostApiTest {
 		requestParam.put("tech_stack", list1);
 		requestParam.put("project", list2);
 		
+		/**
 		//httpRequest.header("Content-type", "application/jason");
 		httpRequest.header("Accept-Encoding", "gzip, deflate, br");
 		httpRequest.header("accept", "application/json");
@@ -71,6 +122,159 @@ public class PostApiTest {
 		int statusCode=response.getStatusCode();
 		System.out.println("Status Code : "+statusCode);
 		Assert.assertEquals(statusCode, 201);
+		**/
+		RestAssured.baseURI="https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1/";   
+		RestAssured.basePath="/Users";
+		given()
+			.header("Accept-Encoding", "gzip, deflate, br")
+			.contentType("application/json")
+			.body(requestParam)
+		.when()
+			.post()
+		.then()
+			.statusCode(201)
+			.log().all();
+		
+		
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test 
+	void postRecord() {
+	
+		requestParam = new JSONObject();
+		requestParam.put("createdAt" , "1631825833");
+		requestParam.put("employee_firstname", "TestData12345");
+		requestParam.put("employee_lastname", "TestData12345");
+		requestParam.put("employee_phonenumbe", "264-783-9453");
+		requestParam.put("ademployee_emaildress", "ademployee_emaildress 1");
+		requestParam.put("citemployee_addressy", "citemployee_addressy 1");
+		requestParam.put("stateemployee_dev_level", "stateemployee_dev_level 1");
+		requestParam.put("employee_gender", "employee_gender 1");
+		requestParam.put("employee_hire_date", "2025-10-31T16:35:45.426Z");
+		requestParam.put("employee_onleave", "true");
+
+		JSONArray techStackList= new JSONArray();
+		JSONArray projectList= new JSONArray();
+		
+		requestParam.put("tech_stack", techStackList);
+		requestParam.put("project", projectList);
+		
+
+		RestAssured.baseURI=bURI;;   
+		RestAssured.basePath=bPath;
+		
+		Response response =given()
+			.header("Accept-Encoding", "gzip, deflate, br")
+			.contentType("application/json")
+			.body(requestParam)
+		.when()
+			.post()
+		.then()
+			.statusCode(201)
+			.log().all().extract().response();
+		
+		emp_id=response.jsonPath().getString("id");
+		System.out.println("Employee ID : "+emp_id);
+		
+	}
+	
+	@Test
+	void deletePostedRecord() {
+		
+		requestParam = new JSONObject();
+		requestParam.put("createdAt" , "1631825833");
+		requestParam.put("employee_firstname", "TestData12345");
+		requestParam.put("employee_lastname", "TestData12345");
+		requestParam.put("employee_phonenumbe", "264-783-9453");
+		requestParam.put("ademployee_emaildress", "ademployee_emaildress 1");
+		requestParam.put("citemployee_addressy", "citemployee_addressy 1");
+		requestParam.put("stateemployee_dev_level", "stateemployee_dev_level 1");
+		requestParam.put("employee_gender", "employee_gender 1");
+		requestParam.put("employee_hire_date", "2025-10-31T16:35:45.426Z");
+		requestParam.put("employee_onleave", "true");
+
+		JSONArray techStackList= new JSONArray();
+		JSONArray projectList= new JSONArray();
+		
+		requestParam.put("tech_stack", techStackList);
+		requestParam.put("project", projectList);
+		
+
+		RestAssured.baseURI=bURI;;   
+		RestAssured.basePath=bPath;
+		
+		Response response =given()
+			.header("Accept-Encoding", "gzip, deflate, br")
+			.contentType("application/json")
+			.body(requestParam)
+		.when()
+			.post()
+		.then()
+			.statusCode(201)
+			.log().all().extract().response();
+		
+		emp_id=response.jsonPath().getString("id");
+		System.out.println("Employee ID : "+emp_id);
+
+		//RestAssured.baseURI=bURI+"/Users/"+emp_id;;   
+		//RestAssured.basePath=bPath;
+		
+		response =given()
+				.baseUri(bURI)
+		.when()
+			.delete("/Users/"+emp_id)
+		.then()
+			.statusCode(200)
+			.log().all().extract().response();
+		
+		//emp_id=response.jsonPath().getString("id");
+		//System.out.println("Employee ID : "+emp_id);
+		
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	@Test 
+	void getEmpID() {
+		RestAssured.baseURI="https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1/";
+		httpRequest=RestAssured.given();
+		
+		requestParam = new JSONObject();
+		requestParam.put("createdAt" , "1631825833");
+		requestParam.put("employee_firstname", "TestData12345");
+		requestParam.put("employee_lastname", "TestData12345");
+		requestParam.put("employee_phonenumbe", "264-783-9453");
+		requestParam.put("ademployee_emaildress", "ademployee_emaildress 1");
+		requestParam.put("citemployee_addressy", "citemployee_addressy 1");
+		requestParam.put("stateemployee_dev_level", "stateemployee_dev_level 1");
+		requestParam.put("employee_gender", "employee_gender 1");
+		requestParam.put("employee_hire_date", "2025-10-31T16:35:45.426Z");
+		requestParam.put("employee_onleave", "true");
+
+		JSONArray techStackList= new JSONArray();
+		JSONArray projectList= new JSONArray();
+		
+		requestParam.put("tech_stack", techStackList);
+		requestParam.put("project", projectList);
+		
+
+		RestAssured.baseURI="https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1/";   
+		RestAssured.basePath="/Users";
+		
+		Response response =given()
+			.header("Accept-Encoding", "gzip, deflate, br")
+			.contentType("application/json")
+			.body(requestParam)
+		.when()
+			.post()
+		.then()
+			.statusCode(201)
+			.log().all().extract().response();
+		
+		emp_id=response.jsonPath().getString("id");
+		System.out.println("Employee ID : "+emp_id);
 		
 	}
 	
@@ -78,19 +282,7 @@ public class PostApiTest {
 	@Test 
 	 void checkStatusCode1() {
 		
-		 map.put("createdAt", "1631825833");
-		 map.put("employee_firstname", "TestData12345");
-		 map.put("employee_lastname", "TestData12345");
-		 map.put("employee_emaildress", "employee_emaildress 1");
-		 map.put("citemployee_addressy", "citemployee_addressy 1");
-		 map.put("stateemployee_dev_level", "stateemployee_dev_level 1");
-		 map.put("employee_gender", "employee_gender 1");
-		 map.put("employee_hire_date", "2025-10-31T16:35:45.426Z");
-		 map.put("employee_onleave", "true");
-		 JSONArray list1= new JSONArray();
-		 JSONArray list2= new JSONArray();
-		 map.put("tech_stack", list1);
-		 map.put("project", list2);
+		
 		    
 		RestAssured.baseURI="https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1/";   
 		RestAssured.basePath="/Users";
@@ -106,5 +298,39 @@ public class PostApiTest {
 		
 		
 	}
+	@Test
+	 void checkStatusCode3() {
+
+		RestAssured.baseURI="https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1/";   
+		RestAssured.basePath="/Users/53";
+		String s =given()
+		.when()
+			.get()
+		.then()
+			.statusCode(200)
+			.extract()
+			.path("employee_firstname");
+			System.out.println(s);
+		
+		
+	}
+	
+	@Test
+	 void checkStatusCode4() {
+
+		RestAssured.baseURI="https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1/";   
+		RestAssured.basePath="/Users/281";
+		String s =given()
+					.when()
+						.get()
+					.then()
+						.statusCode(200)
+					.extract()
+						.jsonPath().getString("employee_firstname");
+			System.out.println(s);
+	}
+	
+	
+	
 
 }
---
+++ com.lcorp.testNG.selenium/src/test/java/apiRestAssured/PostApiTestTest.java
@@ -0,0 +1,27 @@
+package apiRestAssured;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class PostApiTestTest {
+
+  @Test
+  public void checkStatusCodeTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void checkStatusCode1Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void checkStatusCode3Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void checkStatusCode4Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
